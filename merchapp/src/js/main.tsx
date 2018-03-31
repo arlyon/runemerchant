91899bf70902e790461ebf4882f69146b052641c
arlyon <arlyon@me.com>
2018-03-31 13:03:12 +0100

    change naming scheme for clarity
    404, item, search views

--- merchapp/src/js/main.tsx
+++ merchapp/src/js/main.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route} from "react-router";
 import {BrowserRouter as Router, Switch} from "react-router-dom";
-import {FourOFour} from "./views/404";
-import {ItemView} from "./views/item";
-import {SearchView} from "./views/search";
+import {FourOFour} from "./views/404View";
+import {ItemView} from "./views/ItemView";
+import {SearchView} from "./views/SearchView";
 
 class App extends React.Component<{}, {}> {
     public render() {
